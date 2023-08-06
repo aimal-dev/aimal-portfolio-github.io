6ea0ccf4f595305d65d629d4e6bd2e4d61ae0c4b
Aimal Siddique <73801751+Aymal1996@users.noreply.github.com>
2023-08-06 20:22:30 +0500

    Resume completed

---
+++ src/constants/index.js
@@ -0,0 +1,311 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  venture,
+  bizz,
+  Salgo,
+  level,
+  ccc,
+  lga,
+  v10,
+  rasid
+
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "WordPress Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "SEO",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Front End Developer - Internship",
+    company_name: "Bizzworld Communication",
+    icon: bizz,
+    iconBg: "#383E56",
+    date: "Oct 2020 - Jan 2022",
+    points: [
+      "Developing and maintaining web applications using Html,CSS, Bootstrap and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Jr.Front End Developer",
+    company_name: "Level 3 BOS",
+    icon: level,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - June 2022",
+    points: [
+      "Creating and managing web applications with a focus on front-end technologies like WordPress, jQuery, and JavaScript.",
+      "ollaborating with diverse teams, including designers, product managers, and fellow developers, to produce exceptional digital products.",
+      "Ensuring responsive design and compatibility across various browsers for optimal user experiences.",
+      "Engaging in code reviews and offering valuable input to fellow developers to maintain code quality.",
+    ],
+  },
+  {
+    title: "Frontend Developer",
+    company_name: "Centre for Content Creation",
+    icon: ccc,
+    iconBg: "#383E56",
+    date: "Jun 2022 - Jun 2023",
+    points: [
+      "Utilizing a powerful tech stack including Material-UI, Socket.io, WebSocket, Swiper.js, React-redux, styled-components, Ant Design, HTML5, Redux.js, React.js, JavaScript, Cascading Style Sheets (CSS), Bootstrap, and jQuery.",
+      "Collaborating seamlessly within cross-functional teams to deliver high-quality digital products that combine design excellence and cutting-edge technology.",
+      "Ensuring responsive designs and cross-browser compatibility to provide superior user experiences across various platforms.",
+      "Actively engaging in code reviews, fostering a culture of continuous improvement by providing insightful feedback to fellow developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "SleekAlgo",
+    icon: Salgo,
+    iconBg: "#E6DEDD",
+    date: "June 2023 - Present (Part Time)",
+    points: [
+      "Demonstrating expertise in HTML, CSS, jQuery, and JavaScript, with a strong focus on WordPress and basic PHP, to create engaging digital solutions.",
+      "Collaborating effectively within teams to design user-friendly experiences, seamlessly incorporating WordPress features.",
+      "Ensuring designs work well on different devices and browsers for a satisfying user interaction.",
+      "Contributing actively through code reviews, offering valuable insights, and specializing in WordPress and PHP projects.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "VentureHaven",
+    icon: venture,
+    iconBg: "#E6DEDD",
+    date: "July 2023 - Present",
+    points: [
+      "Leveraging an advanced technology stack encompassing Material-UI, Socket.io, WebSocket, Swiper.js, React-redux, styled-components, Ant Design, HTML5, Redux.js, React.js, JavaScript, CSS, Bootstrap, and jQuery, along with a mastery of WordPress development.",
+      "Collaborating seamlessly within multifaceted teams to orchestrate the creation of sophisticated digital solutions that seamlessly merge design excellence with cutting-edge technology.",
+      "Ensuring a seamless, responsive design approach and ensuring cross-browser compatibility, thus delivering unparalleled user experiences across diverse platforms.",
+      "Exemplifying leadership through active participation in code reviews, cultivating a culture of continual enhancement by offering astute feedback to fellow high-level developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "LGA",
+    description:
+      "Empowering learners across the globe through top-tier academic partnerships. Elevating education through unparalleled collaborations for worldwide learners. Global learning excellence fostered by a premier academic partnership.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Swiper JS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "jQuery",
+        color: "green-text-gradient",
+      },
+    ],
+    image: lga,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Rasid",
+    description:
+      "RAISD Software: Elevating Efficiency and Innovation Through Seamless Solutions. Empower Your Digital Transformation with Tailored Software Expertise.Experience Next- Level Performance and Optimization with RAISD Software.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Swiper JS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "jQuery",
+        color: "green-text-gradient",
+      },
+    ],
+    image: rasid,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "V10",
+    description:
+      "nnovative web platform, Project V10, empowers students to explore and manage diverse educational journeys. From bachelor's to PhD programs, with transparent fees, accommodation options, and ELC support.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Swiper JS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "jQuery",
+        color: "green-text-gradient",
+      },
+    ],
+    image: v10,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
